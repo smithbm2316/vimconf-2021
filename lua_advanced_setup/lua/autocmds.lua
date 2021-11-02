948d5263d5229e66e8f5bdadae4e5eb92e2064e8
smithbm2316 <bsmithdev@mailbox.org>
2021-11-02 02:27:35 +0000

    Deploying to gh-pages from @ smithbm2316/vimconf-2021@cdc936ebd4aef5cc03970145b56243d1c118472a 🚀

--- lua_advanced_setup/lua/autocmds.lua
+++
@@ -1,4 +0,0 @@
-local cmd = vim.cmd
-
--- highlight yank for a brief second for visual feedback
-cmd 'autocmd! TextYankPost * lua vim.highlight.on_yank { on_visual = false }'
--- lua_advanced_setup/lua/plugin_configs/init.lua
+++
@@ -1,6 +0,0 @@
-local plugin_config_root = 'plugin_configs.'
-local plugin_config_files = vim.fn.globpath('lua/my/plugs', '*.lua')
-
-for plugin_name in plugin_config_files:gmatch('([%w%d-_]+).lua') do
-  require(plugin_config_root .. plugin_name)
-end
