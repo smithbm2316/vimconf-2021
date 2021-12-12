edd86cf9788b6dfcd2a0090ed4369da2036e5926
Ben Smith <bsmithdev@mailbox.org>
2021-12-11 17:19:28 -0800

    added packer.nvim autoinstall to advanced setup

--- lua_advanced_setup/init.lua
+++ lua_advanced_setup/init.lua
@@ -1,3 +1,10 @@
+-- autoinstall packer.nvim if not already installed
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
+  vim.cmd 'packadd packer.nvim'
+end
+
 -- make sure that globals.lua is required first, as we want to use the
 -- functions and helpers we add there EVERYWHERE in our configuration
 require 'globals'
