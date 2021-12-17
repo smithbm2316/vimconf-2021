f13a6965fa4bb3de4df2fab1a800ba25302c7cdb
Ben Smith <bsmithdev@mailbox.org>
2021-12-17 13:49:29 -0800

    Fix for auto-requiring all of the plugin configs

--- lua_advanced_setup/lua/plugin_configs/init.lua
+++ lua_advanced_setup/lua/plugin_configs/init.lua
@@ -1,6 +1,8 @@
 local plugin_config_root = 'plugin_configs.'
-local plugin_config_files = vim.fn.globpath('lua/my/plugs', '*.lua')
+local plugin_config_files = vim.fn.globpath('lua/plugin_configs', '*.lua')
 
 for plugin_name in plugin_config_files:gmatch('([%w%d-_]+).lua') do
-  require(plugin_config_root .. plugin_name)
+  if plugin_name ~= 'init' then
+    require(plugin_config_root .. plugin_name)
+  end
 end
